ae824916d3cd6e6a6d817efbe381fbb97f7a90d1
Pedro Dalvy <pedrodalvy@outlook.com>
2021-01-09 13:04:48 -0400

    [factory] criada factory para produtos

---
+++ app/Domain/Enuns/ProdutoEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domain\Enuns;
+
+class ProdutoEnum
+{
+    public const PRODUTO_SIMPLES = 1;
+    public const PRODUTO_DIGITAL = 2;
+    public const PRODUTO_CONFIGURAVEL = 3;
+    public const PRODUTO_AGRUPADO = 4;
+}
---
+++ app/Domain/Factories/ProdutosPorTipoFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Domain\Factories;
+
+use App\Domain\Enuns\ProdutoEnum;
+use App\Domain\Services\Produtos\ProdutosPorTipo\ProdutosAgrupadosService;
+use App\Domain\Services\Produtos\ProdutosPorTipo\ProdutosConfiguraveisService;
+use App\Domain\Services\Produtos\ProdutosPorTipo\ProdutosDigitaisService;
+use App\Domain\Services\Produtos\ProdutosPorTipo\ProdutosPorTipoAbstract;
+use App\Domain\Services\Produtos\ProdutosPorTipo\ProdutosSimplesService;
+
+class ProdutosPorTipoFactory
+{
+    public static function factory(int $tipoProduto): ProdutosPorTipoAbstract
+    {
+        switch ($tipoProduto) {
+            case ProdutoEnum::PRODUTO_SIMPLES:
+                return app(ProdutosSimplesService::class);
+            case ProdutoEnum::PRODUTO_DIGITAL:
+                return app(ProdutosDigitaisService::class);
+            case ProdutoEnum::PRODUTO_CONFIGURAVEL:
+                return app(ProdutosConfiguraveisService::class);
+            case ProdutoEnum::PRODUTO_AGRUPADO:
+                return app(ProdutosAgrupadosService::class);
+        }
+    }
+}
