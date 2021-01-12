c90ffe1fe017ddf2520ea6f872472d46459443b4
Pedro Dalvy <pedrodalvy@outlook.com>
2021-01-11 21:44:33 -0400

    [enum] adicionada descricao do tipo de produto no enum

--- app/Domain/Enums/ProdutoEnum.php
+++ app/Domain/Enums/ProdutoEnum.php
@@ -10,4 +10,17 @@ class ProdutoEnum extends Enum
     public const PRODUTO_DIGITAL = 2;
     public const PRODUTO_CONFIGURAVEL = 3;
     public const PRODUTO_AGRUPADO = 4;
+
+    public const DESCRICOES = [
+        self::PRODUTO_SIMPLES => 'Produto Simples',
+        self::PRODUTO_DIGITAL => 'Produto Digital',
+        self::PRODUTO_CONFIGURAVEL => 'Produto Configurável',
+        self::PRODUTO_AGRUPADO => 'Produto Agrupado',
+    ];
+
+
+    public static function getDescription($value): string
+    {
+        return self::DESCRICOES[$value] ?? '';
+    }
 }
